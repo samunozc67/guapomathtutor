8a579122e16219a75f78da95bbf25110cf5ae174
samuel ext <samuelext@samuel-3.local>
2026-04-06 12:33:31 -0500

    Revert "fix: restore and update Tutor.tsx with gemini-2.5-flash"
    This reverts commit 4f1248bf0a5c468a88c3150039e47779b5a8a428, reversing
    changes made to 14b36dc166ca20640f4987b3d620299e509c4d9e.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { AuthProvider } from './contexts/AuthContext'
-import { LangProvider } from './contexts/LangContext'
-import './styles.css'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <LangProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </LangProvider>
-  </React.StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
